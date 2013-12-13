687fa6e85f8723b13e5a92da6b44fffbf0acf650
codered008 <daniel.brown2020@gmail.com>
2013-12-13 14:29:14 -0600

    Haaalp

--- City_Saver/City_Saver/ObjectClasses/FireShooter.cs
+++ City_Saver/City_Saver/ObjectClasses/FireShooter.cs
@@ -15,11 +15,14 @@ namespace City_Saver.ObjectClasses
     class FireShooter: ObjectClasses.People
     {
         Animation.Animation fireShooterAni;
+        Vector2 shooterPos;
         float currentTime = 0;
         float timeDuration = 1f;
         bool isEnemy = true; //registers the object as an enemy to the player
         bool isAlive = true;
-        //Firebullet object here
+        int counter = 0;
+        Bullet[] fireBullets = new Bullet[30];//the bullets the enemy will shoot
+        Bullet bullet = new Bullet();
 
         public FireShooter()
         {
@@ -39,10 +42,29 @@ namespace City_Saver.ObjectClasses
         }
         public void setSpriteAnimation(ContentManager content, String assetName, int frameNumber, Vector2 position)
         {
+            setPosition(position);
             fireShooterAni = new Animation.Animation(content, assetName, 0.3f, frameNumber, position);
         }
 
+<<<<<<< HEAD
         //public void createBullet
+=======
+        public void setPosition(Vector2 pos)
+        {
+            shooterPos = pos;
+        }
+        public void createBullets(ContentManager content)
+        {
+            String assetName = "Sprites\\FireBullet";
+            bullet.setBulletAnimation(content, assetName, 1, shooterPos);
+
+            //Adds an amount of bullets to the enemy's pack
+            for (int i = 0; i < 30; i++)
+            {
+                fireBullets[i] = bullet;
+            }
+        }
+>>>>>>> c17852dfa929e421a3fbfced4fd604f52de4a7d2
          
         public void Update(GameTime gameTime)
         {
@@ -52,16 +74,39 @@ namespace City_Saver.ObjectClasses
             {
                 isAlive = false;
             }
-
-            currentTime += (float)gameTime.ElapsedGameTime.TotalSeconds;
-            if (currentTime >= timeDuration)
+            else
             {
-                //Shoot a bullet
-                //Put code here
-                currentTime -= timeDuration;
+                
+                foreach (Bullet b in fireBullets)
+                {
+                    b.Update(gameTime); //updates the bullet's actions. If it has been fired, then the bullet will be moving
+                }
+                currentTime += (float)gameTime.ElapsedGameTime.TotalSeconds;
+                if (currentTime >= timeDuration)
+                {
+                    //Shoot a bullet
+                    //Put code here
+                    if (counter >= fireBullets.Length)
+                    {
+                        //reset the counter once it hits the max number of elements
+                        //in the array
+                        counter = 0;
+                    }
+                    //Sets the bullet to active
+                    if (!fireBullets[counter].getBulletStatus())
+                    {
+                        fireBullets[counter].setActive();
+                    }
+                    currentTime -= timeDuration;
+                    counter++;//increment counter
+                }
             }
 
 
         }
+
+        //public void resetBullets(){
+        //    foreach(Bullet b in fireBullets){
+        //        //reset the bullet's status
     }
 }
