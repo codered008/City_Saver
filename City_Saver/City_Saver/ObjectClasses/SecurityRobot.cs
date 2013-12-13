b5762a964dfa4f7c044646ce8022d65a25d5ad00
codered008 <daniel.brown2020@gmail.com>
2013-12-13 10:42:21 -0600

    Added enemy classes
    Added some enemy classes, changed the names of the enemies.  I'm saving
    the enemy ship as a potential boss

---
+++ City_Saver/City_Saver/ObjectClasses/SecurityRobot.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace City_Saver.ObjectClasses
+{
+    class SecurityRobot : ObjectClasses.People
+    {
+        Animation.Animation SecurityRobotAni;
+        float currentTime = 0;
+        float timeDuration = 1f;
+        Vector2 enemyPosition;
+        bool isEnemy = true; //registers the object as an enemy to the player
+
+        public SecurityRobot(Vector2 position)
+        {
+            Health = 20;
+            Magic = 0;
+            this.enemyPosition = position;
+        }
+
+        public void hpDamage(int damage)
+        {
+            Health -= damage;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            //Has the monster shoot a bullet
+            currentTime += (float)gameTime.ElapsedGameTime.TotalSeconds;
+            if (currentTime >= timeDuration)
+            {
+                //Shoot a bullet
+                //Put code here
+                currentTime -= timeDuration;
+            }
+        }
+    }
+}
---
+++ City_Saver/City_Saver/ObjectClasses/Trooper.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+
+namespace City_Saver.ObjectClasses
+{
+    class Trooper : ObjectClasses.People
+    {
+        Animation.Animation TrooperAni;
+        float currentTime = 0;
+        float timeDuration = 1f;
+        Vector2 enemyPosition;
+        bool isEnemy = true; //registers the object as an enemy to the player
+
+        public Trooper(Vector2 position)
+        {
+            Health = 40;
+            Magic = 20;
+            this.enemyPosition = position;
+        }
+
+        public void hpDamage(int damage)
+        {
+            Health -= damage;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            //Has the monster shoot a bullet
+            currentTime += (float)gameTime.ElapsedGameTime.TotalSeconds;
+            if (currentTime >= timeDuration)
+            {
+                //Shoot a bullet
+                //Put code here
+                enemyPosition.X -= 0.5f * currentTime;      //Need to test this
+                currentTime -= timeDuration;
+            }
+        }
+    }
+}
